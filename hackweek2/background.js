b9326301bf5cc62291e304b91ac7c45af8a10d82
Sean Conaty <sean.conaty@remind101.com>
2016-10-06 13:00:34 -0700

    cleanup

--- hackweek2/background.js
+++ hackweek2/background.js
@@ -1,6 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-	var newURL = "https://www.remind.com/messages/new?body="+ encodeURIComponent(activeTab.title)+ "%3A%20" + encodeURIComponent(activeTab.url) ;
-	chrome.tabs.create({ url: newURL });
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+  var url = "https://www.remind.com/messages/new?body=" + encodeURIComponent(activeTab.title) + "%3A%20" + encodeURIComponent(activeTab.url);
+  chrome.tabs.create({url: url});
 });
-
-
