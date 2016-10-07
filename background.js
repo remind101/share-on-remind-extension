2f978e766c8100be71891c9caef45eab60469600
connor-pierson <connor@remind101.com>
2016-10-07 14:17:07 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-  var url = "https://www.remind.com/messages/new?utm_source=chrome_extension&utm_medium=site&utm_campaign=content_share&body=" + encodeURIComponent(activeTab.title + ": " + activeTab.url);
+  var url = "https://www.remind.com/messages/new?utm_source=chrome_extension&utm_medium=site&utm_campaign=content_share&body=" + encodeURIComponent(activeTab.title + " " + activeTab.url);
   chrome.tabs.create({url: url});
 });
